61f09b5fe53fd3f63bd9bd6aaafc6fd1ebd99f6c
Sachin Jangid <76869950+Sachin21393@users.noreply.github.com>
2022-05-19 20:13:26 +0530

    Add files via upload

---
+++ public/css/js/connect.js
@@ -0,0 +1,11 @@
+// var socket;
+function f1(){
+
+
+// socket=io.connect('http://localhost:3000');  //connect to server
+// socket.on('connect',function(){
+//     console.log('connected');
+//     });
+    console.log("hello");
+
+}
